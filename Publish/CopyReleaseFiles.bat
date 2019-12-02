e3e06c276750e40657bf05b4dea4da9b0dda0e7c
limingsheng03 <limingsheng03@countrygarden.com.cn>
2019-12-02 21:16:16 +0800

    添加资源

--- Publish/CopyReleaseFiles.bat
+++ Publish/CopyReleaseFiles.bat
@@ -6,6 +6,8 @@ copy /Y /V ..\x64\Release\license.lst .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\18\x64\
+copy /Y /V ..\x64\Release\BGYHackARX18.arx .\out\Support\18\x64\
+copy /Y /V ..\x64\Release\Boundary.dll .\out\Support\18\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\19\x64\
@@ -13,6 +15,8 @@ copy /Y /V ..\x64\Release\license.lst .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\19\x64\
+copy /Y /V ..\x64\Release\BGYHackARX19.arx .\out\Support\19\x64\
+copy /Y /V ..\x64\Release\Boundary.dll .\out\Support\19\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\20\x64\
@@ -20,6 +24,8 @@ copy /Y /V ..\x64\Release\license.lst .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\20\x64\
+copy /Y /V ..\x64\Release\BGYHackARX20.arx .\out\Support\20\x64\
+copy /Y /V ..\x64\Release\Boundary.dll .\out\Support\20\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad21.arx .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\21\x64\
@@ -27,6 +33,7 @@ copy /Y /V ..\x64\Release\license.lst .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\21\x64\
+copy /Y /V ..\x64\Release\Boundary.dll .\out\Support\21\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad22.arx .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\22\x64\
@@ -34,3 +41,4 @@ copy /Y /V ..\x64\Release\license.lst .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\22\x64\
+copy /Y /V ..\x64\Release\Boundary.dll .\out\Support\22\x64\
