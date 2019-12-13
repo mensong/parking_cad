b5e630f6626f092707732d4d543d03ba8ff26143
limingsheng03 <limingsheng03@countrygarden.com.cn>
2019-12-13 09:54:23 +0800

    安装包增加exchange.ico

--- Publish/CopyReleaseFiles.bat
+++ Publish/CopyReleaseFiles.bat
@@ -14,6 +14,7 @@ copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\BGYBlockManager18.arx .\out\Support\18\x64\
+copy /Y /V ..\x64\Release\exchange.ico .\out\Support\18\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\19\x64\
@@ -29,6 +30,7 @@ copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\BGYBlockManager19.arx .\out\Support\19\x64\
+copy /Y /V ..\x64\Release\exchange.ico .\out\Support\19\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\20\x64\
@@ -44,6 +46,7 @@ copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\BGYBlockManager20.arx .\out\Support\20\x64\
+copy /Y /V ..\x64\Release\exchange.ico .\out\Support\20\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad21.arx .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\21\x64\
@@ -58,6 +61,7 @@ copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\BGYBlockManager21.arx .\out\Support\21\x64\
+copy /Y /V ..\x64\Release\exchange.ico .\out\Support\21\x64\
 
 copy /Y /V ..\x64\Release\BGYParking_cad22.arx .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\LibcurlHttp.dll .\out\Support\22\x64\
@@ -72,3 +76,4 @@ copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\BGYBlockManager22.arx .\out\Support\22\x64\
+copy /Y /V ..\x64\Release\exchange.ico .\out\Support\22\x64\
