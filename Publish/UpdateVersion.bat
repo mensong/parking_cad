a4b4f69ba1c8b4297a8a4edbc34e918d56435701
limingsheng03 <limingsheng03@countrygarden.com.cn>
2020-01-17 15:33:38 +0800

    安装包更名

--- Publish/UpdateVersion.bat
+++ Publish/UpdateVersion.bat
@@ -4,6 +4,6 @@ set logfile=tmp_ver.txt
 
 echo ============================ %~n0 ============================>%logfile%
 
-UpdateSetupFactoryVersion -f Install.suf -s "<Filename>碧桂园车库智能设计系统V" -e ".exe</Filename>">>%logfile%
+UpdateSetupFactoryVersion -f Install.suf -s "<Filename>SetupV" -e ".exe</Filename>">>%logfile%
 
 ..\webclient.vbs "http://127.0.0.1:8000/devops-cpp/concat-var" "%logfile%"
