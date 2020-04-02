654f778c2d1441c4db1e86c6754a8c93c912767c
张泽池 <zhangzechi@countrygarden.com.cn>
2020-04-02 09:04:35 +0800

    增加数据库参数，以及修改模板中轴号块

--- Parking_cad/CommonFuntion.h
+++ Parking_cad/CommonFuntion.h
@@ -22,7 +22,7 @@ public:
 	//模糊匹配,chars1字段是否在chars2中
 	static bool string1_In_string2(const wchar_t* chars1, const wchar_t* chars2);
 	//获取指定图层上所有实体ID
-	static AcDbObjectIdArray GetAllEntityId(const TCHAR* layername);
+	static AcDbObjectIdArray GetAllEntityId(const TCHAR* layername, AcDbDatabase *pDb = acdbCurDwg());
 	static void SaveEntInfo(AcDbObjectIdArray& inputIds, AcGePoint2dArray& points);
 	//处理相交的实体，使离交点最近的端点设置成交点的值
 	static void DealEnt(AcDbEntity* pEnt, AcGePoint3dArray& intersectPoints);
@@ -36,7 +36,7 @@ public:
 	//插入块参照后，将块内对象坐标换算为其当前dwg中的坐标
 	static void GetEntPointofinBlock();
 	//画多段线
-	static void DrowPloyLine(AcGePoint2dArray& inputpoints);
+	static void DrowPloyLine(AcGePoint2dArray& inputpoints, AcDbDatabase *pDb = acdbCurDwg());
 	//画直线
 	static AcDbObjectIdArray DrowLine(AcGePoint3dArray& inputpoints);
 	//得到线之间所有的交点
@@ -45,9 +45,9 @@ public:
 	//将所有交点进行排序，取得左下、左上、右上、右下角的点
 	static void GetCornerPoint(AcGePoint3dArray& inputptArr,AcGePoint3d& lefdownPt,AcGePoint3d& lefupPt,AcGePoint3d& rigthupPt,AcGePoint3d& rigthdownPt);
 	//确定方向和两点，画尺寸标注
-	static void DrowDimaligned(const AcString& setLayerName, AcGePoint3d& point1, AcGePoint3d& point2);
+	static void DrowDimaligned(const AcString& setLayerName, AcGePoint3d& point1, AcGePoint3d& point2, AcDbDatabase *pDb = acdbCurDwg());
 	//画尺寸标注
-	static AcDbObjectId CreateDimAligned(const AcGePoint3d& pt1, const AcGePoint3d& pt2, const AcGePoint3d& ptLine,/*const AcGeVector3d& vecOffset,*/ const ACHAR* dimText);
+	static AcDbObjectId CreateDimAligned(const AcGePoint3d& pt1, const AcGePoint3d& pt2, const AcGePoint3d& ptLine,const ACHAR* dimText, AcDbDatabase *pDb= acdbCurDwg());
 	//根据相对直角坐标来计算一个点的位置：
 	static AcGePoint3d RelativePoint(const AcGePoint3d& pt, double x, double y);
 	//求线段长度
@@ -69,7 +69,7 @@ public:
 	//利用线选，所选择的实体是否在指定图层
 	static bool IsIntersectEnt(const ACHAR *LineLayerName, AcGePoint3d& pt1, AcGePoint3d& pt2,AcRxClass* classDesc);
 	//获取当前图层所有文字样式
-	static void GetALLTextStyle(std::vector<CString>& textStyle);
+	static void GetALLTextStyle(std::vector<CString>& textStyle, AcDbDatabase *pDb = acdbCurDwg());
 	////字符串分割到数组
 	static void Split(const std::string& src, const std::string& separator, std::vector<std::string>& dest);
 	static void Split(const std::string& src, const std::string& separator, std::string& dest);
@@ -78,13 +78,13 @@ public:
 	//判断字符串是否为gbk
 	static bool is_str_gbk(const char* str);
 	//从外部DWG文件中导入带有属性的块参照到当前图纸中
-	static bool InserBlockFormDWG(const CString& dwgblockname, const CString& dwgpath, AcGePoint3d& inserpoint,AcDbObjectId& outinserblockid);
+	static bool InserBlockFormDWG(const CString& dwgblockname, const CString& dwgpath, AcGePoint3d& inserpoint,AcDbObjectId& outinserblockid, AcDbDatabase *pDb = acdbCurDwg());
 	//获取指定数据库中指定块表中的所有实体的ID
 	static int getIdsByDwgBlkName(AcDbDatabase *pDwg, CString strBlkName, AcDbObjectIdArray &objIds);
-	static void setLayer(const CString& layerName, const int& layerColor);
+	static void setLayer(const CString& layerName, const int& layerColor, AcDbDatabase *pDb = acdbCurDwg());
 	//车道轴网尺寸标注
-	static void creatLaneGridDimensionsDimStyle(const CString& stylename);
-	static void setEntityLayer(const AcString& setlayername, AcDbObjectId& entityId);
+	static void creatLaneGridDimensionsDimStyle(const CString& stylename, AcDbDatabase *pDb = acdbCurDwg());
+	static void setEntityLayer(const AcString& setlayername, AcDbObjectId& entityId, AcDbDatabase *pDb = acdbCurDwg());
 	
 public:
 	/*
--- Parking_cad/OperaAxleNetMaking.h
+++ Parking_cad/OperaAxleNetMaking.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "IOperaLog.h"
 
-
 typedef signed char        int8_t;
 typedef short              int16_t;
 typedef int                int32_t;
@@ -27,14 +26,18 @@ public:
 	~COperaAxleNetMaking();
 	virtual void Start();
 
+	static void drawAxisNumber(AcDbObjectIdArray& LineIds,std::map<AcDbObjectId,AcString>& AxisNumberMap, AcDbDatabase *pDb = acdbCurDwg());
+
 public:
-	static void InitialProcessing(const CString& layerNameofAXSI,AcDbObjectIdArray& sortIds,AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt, std::vector<AcDbObjectId>& outputIds);
-	static void DrowBigDimaligned(const CString& layerNameofAXSI,std::vector<AcDbObjectId>& inputids, AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt);
+	static void InitialProcessing(const CString& layerNameofAXSI,AcDbObjectIdArray& sortIds,AcGePoint3d& inputstartpt
+		, AcGePoint3d& inputendpt, std::vector<AcDbObjectId>& outputIds, AcDbDatabase *pDb = acdbCurDwg());
+	static void DrowBigDimaligned(const CString& layerNameofAXSI,std::vector<AcDbObjectId>& inputids, AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt, AcDbDatabase *pDb = acdbCurDwg());
 	static AcDbObjectIdArray FilterIds(AcDbObjectIdArray& inputids, std::vector<AcDbObjectId>& inputids1, std::vector<AcDbObjectId>& inputids2);
 	static AcDbObjectIdArray FilterIds(AcDbObjectIdArray& inputtotalids, std::vector<AcDbObjectId>& inputids);
 	static void AxisLineInfo(AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt, AcGePoint3d& inputSecondstartpt, AcGePoint3d& inputSecondendpt, AcGeVector3d& projectvect_90, AcGeVector3d& projectvect_180,
 		AcGePoint3d& startpt, AcGePoint3d& endpt, AcGePoint3d& outpt1, AcGePoint3d& outpt2, AcGePoint3d& outpoint);
-	static void SpecialDeal(const CString& layerNameofAXSI,AcDbObjectIdArray& inputIds, AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt, AcGePoint3d& inputSecondstartpt, AcGePoint3d& inputSecondendpt, const ACHAR *LineLayerName);
+	static void SpecialDeal(const CString& layerNameofAXSI,AcDbObjectIdArray& inputIds, AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt
+		, AcGePoint3d& inputSecondstartpt, AcGePoint3d& inputSecondendpt, const ACHAR *LineLayerName, AcDbDatabase *pDb = acdbCurDwg());
 	//add by yangbing use set axisId data
 	static void setAxisIds(AcDbObjectIdArray axisIds);
 	static AcDbObjectIdArray ms_axisIds;
@@ -43,17 +46,20 @@ public:
 	static double getBlockRadius(AcDbObjectId& blockid, double scaleFactor, AcGePoint3d& basept);
 	static void getLinePoint(AcDbObjectId& lineid, AcGePoint3d& startpt, AcGePoint3d& endpt);
 	static AcGePoint3d getChangPoint(AcGePoint3d& basept, AcGeVector3d& moveagvec, double movedis);
-	static void inserAadAxleNum(AcDbObjectIdArray& sortIds, AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt, int casenum, int plusorminus = 1);
+	static void inserAadAxleNum(AcDbObjectIdArray& sortIds, AcGePoint3d& inputstartpt, AcGePoint3d& inputendpt
+		, /*int casenum,*/ int plusorminus ,std::map<AcDbObjectId,AcString>& AxisNumberMap, AcDbDatabase *pDb = acdbCurDwg());
 	static void inserBlockRec(const AcString& sBlockName, AcGePoint3d& basept, AcGeVector3d& moveagvec, double circleradius
-		, const AcString& tagvalue);
-	static void dealBlock(const AcString& sBlockName, AcGePoint3d& startpt, AcGeVector3d& moveagvec1
-		, AcGePoint3d& endpt, AcGeVector3d& moveagvec2, double scaleFactor, double circleradius, const AcString& tagvalue);
+		, const AcString& tagvalue, AcDbDatabase *pDb = acdbCurDwg());
+	static void dealBlock(const AcString& sBlockName, AcGePoint3d& startpt, AcGeVector3d& moveagvec1, AcGePoint3d& endpt
+		, AcGeVector3d& moveagvec2, double scaleFactor, double circleradius, const AcString& tagvalue, AcDbDatabase *pDb = acdbCurDwg());
 	//纵向编号,tagnum为个位字母，addtagnum为十位字母，tagvalue为个位字母值，csAddtagvalue为十位字母值
 	static void LongitudinalNumbering(int& tagnum, int& addtagnum, CString& tagvalue, CString& csAddtagvalue);
 	//创建折线，scaleFactor为扩大比例值，plusorminus偏折的角度（只有1和-1）
 	static void creatOtherGuideLine(AcGePoint3d& startpt, AcGePoint3d& endpt, AcGePoint3d& movept1, AcGeVector3d& moveagvec1
-		, AcGePoint3d& movept2, AcGeVector3d& moveagvec2, double scaleFactor, int plusorminus = 1);
+		, AcGePoint3d& movept2, AcGeVector3d& moveagvec2, double scaleFactor, int plusorminus = 1, AcDbDatabase *pDb = acdbCurDwg());
 	//设置实体的颜色
 	static void setEntColor(AcDbObjectId& id, uint16_t colornum);
-	static AcDbObjectId DrowLine(AcGePoint3d& starpt, AcGePoint3d& endpt);
+	static AcDbObjectId DrowLine(AcGePoint3d& starpt, AcGePoint3d& endpt, AcDbDatabase *pDb = acdbCurDwg());
+
+	static AcString getTagvaluefromMap(std::map<AcDbObjectId, AcString>& AxisNumberMap, AcDbObjectId& id);
 };
