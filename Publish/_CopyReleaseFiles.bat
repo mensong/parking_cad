d62039fe320c26af894ee197903fca8a290a4d3a
limingsheng03 <limingsheng03@countrygarden.com.cn>
2020-05-28 15:32:53 +0800

    git合规化

--- Publish/_CopyReleaseFiles.bat
+++ Publish/_CopyReleaseFiles.bat
@@ -12,70 +12,54 @@ mkdir .\out\Support\21\x64\
 mkdir .\out\Support\22\x86\
 mkdir .\out\Support\22\x64\
 
-copy /Y /V ..\x64\Release\BGYParking_cad18.arx .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\18\x64\
-copy /Y /V ..\x64\Release\BGYHackARX18.arx .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\Mapsign.dwg .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\18\x64\
-copy /Y /V ..\x64\Release\BGYBlockManager18.arx .\out\Support\18\x64\
-copy /Y /V ..\x64\Release\BGYNotice18.arx .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\exchange.ico .\out\Support\18\x64\
+copy /Y /V ..\x64\Release\*18.arx .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\*.dll .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\*.exe .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\*.ini .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\*.LIN .\out\Support\18\x64\
-copy /Y /V ..\x64\Release\BGYBuildingLib18.arx .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\NoPicture.bmp .\out\Support\18\x64\
 copy /Y /V ..\x64\Release\Notice.html .\out\Support\18\x64\
 
-
-copy /Y /V ..\x64\Release\BGYParking_cad19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\19\x64\
-copy /Y /V ..\x64\Release\BGYHackARX19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\Mapsign.dwg .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\19\x64\
-copy /Y /V ..\x64\Release\BGYBlockManager19.arx .\out\Support\19\x64\
-copy /Y /V ..\x64\Release\BGYNotice19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\exchange.ico .\out\Support\19\x64\
+copy /Y /V ..\x64\Release\*19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\*.dll .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\*.exe .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\*.ini .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\*.LIN .\out\Support\19\x64\
-copy /Y /V ..\x64\Release\BGYBuildingLib19.arx .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\NoPicture.bmp .\out\Support\19\x64\
 copy /Y /V ..\x64\Release\Notice.html .\out\Support\19\x64\
 
-
-copy /Y /V ..\x64\Release\BGYParking_cad20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\20\x64\
-copy /Y /V ..\x64\Release\BGYHackARX20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\Mapsign.dwg .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\20\x64\
-copy /Y /V ..\x64\Release\BGYBlockManager20.arx .\out\Support\20\x64\
-copy /Y /V ..\x64\Release\BGYNotice20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\exchange.ico .\out\Support\20\x64\
+copy /Y /V ..\x64\Release\*20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\*.dll .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\*.exe .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\*.ini .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\*.LIN .\out\Support\20\x64\
-copy /Y /V ..\x64\Release\BGYBuildingLib20.arx .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\NoPicture.bmp .\out\Support\20\x64\
 copy /Y /V ..\x64\Release\Notice.html .\out\Support\20\x64\
 
-
-copy /Y /V ..\x64\Release\BGYParking_cad21.arx .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\21\x64\
@@ -83,19 +67,15 @@ copy /Y /V ..\x64\Release\Mapsign.dwg .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\21\x64\
-copy /Y /V ..\x64\Release\BGYBlockManager21.arx .\out\Support\21\x64\
-copy /Y /V ..\x64\Release\BGYNotice21.arx .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\exchange.ico .\out\Support\21\x64\
+copy /Y /V ..\x64\Release\*21.arx .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\*.dll .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\*.exe .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\*.ini .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\*.LIN .\out\Support\21\x64\
-copy /Y /V ..\x64\Release\BGYBuildingLib21.arx .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\NoPicture.bmp .\out\Support\21\x64\
 copy /Y /V ..\x64\Release\Notice.html .\out\Support\21\x64\
 
-
-copy /Y /V ..\x64\Release\BGYParking_cad22.arx .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\loading.gif .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\template.dwg .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\ParkingConfig.json .\out\Support\22\x64\
@@ -103,14 +83,12 @@ copy /Y /V ..\x64\Release\Mapsign.dwg .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\parking_cad.cuix .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\parking_cad.mnr .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\parking_cad_light.mnr .\out\Support\22\x64\
-copy /Y /V ..\x64\Release\BGYBlockManager22.arx .\out\Support\22\x64\
-copy /Y /V ..\x64\Release\BGYNotice22.arx .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\exchange.ico .\out\Support\22\x64\
+copy /Y /V ..\x64\Release\*22.arx .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\*.dll .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\*.exe .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\*.ini .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\*.LIN .\out\Support\22\x64\
-copy /Y /V ..\x64\Release\BGYBuildingLib22.arx .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\NoPicture.bmp .\out\Support\22\x64\
 copy /Y /V ..\x64\Release\Notice.html .\out\Support\22\x64\
 
